232ea457a2b23ab2ccca7bc2a4c3f57b1f970ada
Sam Ban <sam@gpmd.co.uk>
2018-09-11 08:53:13 +0100

    registering and calling hooks

--- log.go
+++ log.go
@@ -8,23 +8,32 @@ import (
 )
 
 const (
-	infoLog    = 1
-	warningLog = 3
-	errorLog   = 7
-	fatalLog   = 15
+	InfoLog = 1 << iota
+	WarningLog
+	ErrorLog
+	FatalLog
 )
 
+// Hook is a callback function
+type Hook func(message string) error
+
 var logs []string
+var hooks map[int][]Hook
+
+// RegisterHook will execute given hook function on every message
+func RegisterHook(h Hook, level int) {
+	hooks[level] = append(hooks[level], h)
+}
 
 func truncateString(str string, num int) string {
-	bnoden := str
+	s := str
 	if len(str) > num {
 		if num > 3 {
 			num -= 3
 		}
-		bnoden = str[0:num] + "..."
+		s = str[0:num] + "..."
 	}
-	return bnoden
+	return s
 }
 
 // Send sends message to the channel with ISO timestamp
@@ -36,20 +45,39 @@ func store(severity int, message string, a ...interface{}) {
 
 // Infof logs to dashboard (sends message through log channel) plus echoes to standard output
 func Infof(message string, a ...interface{}) {
-	if len(a) > 0 {
-		message = truncateString(fmt.Sprintf(message, a...), 5000)
-	}
-	log.Println(message)
-	store(infoLog, message)
+	f(InfoLog, message, a)
+}
+
+// Warningf logs to dashboard (sends message through log channel) plus echoes to standard output
+func Warningf(message string, a ...interface{}) {
+	f(WarningLog, message, a...)
 }
 
 // Errorf logs to dashboard (sends message through log channel) plus echoes to standard output
 func Errorf(message string, a ...interface{}) {
+	f(ErrorLog, message, a...)
+}
+
+// Fatalf logs to dashboard (sends message through log channel) plus echoes to standard output
+func Fatalf(message string, a ...interface{}) {
+	f(FatalLog, message, a...)
+}
+
+func f(level int, message string, a ...interface{}) {
+	if len(hooks[level]) > 0 {
+		for _, h := range hooks[level] {
+			err := h(fmt.Sprintf(message, a...))
+			if err != nil {
+				log.Printf("Log: hook returned error %#v %v\n", h, err)
+				store(level, fmt.Sprintf("Log: hook returned error %#v %v\n", h, err))
+			}
+		}
+	}
 	if len(a) > 0 {
 		message = truncateString(fmt.Sprintf(message, a...), 5000)
 	}
 	log.Println(message)
-	store(errorLog, message)
+	store(level, message)
 }
 
 // Filter returns logs for minimum level
@@ -61,4 +89,8 @@ func Filter(minlevel int) (ret []string) {
 		}
 	}
 	return
+}
+
+func init() {
+	hooks = make(map[int][]Hook, 1)
 }
