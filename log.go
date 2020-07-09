11fa5e0448a9ca1ba0f867386fc6ab7909ad1408
Sam Ban <sam@gpmd.co.uk>
2020-07-09 22:02:32 +0100

    get back default colours

--- log.go
+++ log.go
@@ -144,7 +144,7 @@ func init() {
 
 	log.SetFormatter(&log.TextFormatter{
 		DisableColors:   false,
-		ForceColors:     false,
+		ForceColors:     true,
 		TimestampFormat: "2006-01-02 15:04:05",
 		FullTimestamp:   true,
 	},
